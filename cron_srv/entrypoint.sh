9bf19f9741cea1f33bcc3e62d3d2b5e1b52e0a6e
Simon <44276098+Simon248@users.noreply.github.com>
2024-08-09 02:19:38 +0200

    ajout nettoyage variable CRON_SCHEDULE avant usage

--- cron_srv/entrypoint.sh
+++ cron_srv/entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+#nettoyer la variable d'env CRON_SCHEDULE
+CLEAN_CRON_SCHEDULE=$(echo "$CRON_SCHEDULE" | sed 's/[^a-zA-Z0-9* /]*//g')
+
 # Ajouter la tâche cron depuis la variable d'environnement
-echo "$CRON_SCHEDULE /usr/local/bin/python3 /build_doc.py >> /var/log/cron.log 2>&1" | crontab -
+echo "$CLEAN_CRON_SCHEDULE /usr/local/bin/python3 /build_doc.py >> /var/log/cron.log 2>&1" | crontab -
 
 # Exécuter la commande immédiatement au démarrage du conteneur
 /usr/local/bin/python3 /build_doc.py
